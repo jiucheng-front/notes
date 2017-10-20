23188a589eeefba79c7ab10b593b3379dffd7c04
wangjianfei <444128852@qq.com>
2017-10-20 10:33:45 +0800

    update the factory.js

--- factory.js
+++ factory.js
@@ -207,29 +207,83 @@ function getUrlParameter(strParame){
 var key=getUrlParameter("key");//10
 var id=getUrlParameter("hello");//10
 
+// 八、简单的密码加密和解密
+
+// 加密
+function encrypto( str, xor, hex ) {
+    if ( typeof str !== 'string' || typeof xor !== 'number' || typeof hex !== 'number') {
+      return;
+    }
+    let resultList = []; 
+    hex = hex <= 25 ? hex : hex % 25;
+    for ( let i=0; i<str.length; i++ ) {
+      // 提取字符串每个字符的ascll码
+      let charCode = str.charCodeAt(i);
+      // 进行异或加密
+      charCode = (charCode * 1) ^ xor;
+      // 异或加密后的字符转成 hex 位数的字符串
+      charCode = charCode.toString(hex);
+      resultList.push(charCode);
+    }
+    let splitStr = String.fromCharCode(hex + 97);
+    let resultStr = resultList.join( splitStr );
+    return resultStr;
+}
+//解密
+function decrypto( str, xor, hex ) { 
+    if ( typeof str !== 'string' || typeof xor !== 'number' || typeof hex !== 'number') {
+      return;
+    }
+    let strCharList = [];
+    let resultList = []; 
+    hex = hex <= 25 ? hex : hex % 25;
+    // 解析出分割字符
+    let splitStr = String.fromCharCode(hex + 97);
+    // 分割出加密字符串的加密后的每个字符
+    strCharList = str.split(splitStr);
+    for ( let i=0; i<strCharList.length; i++ ) {
+      // 将加密后的每个字符转成加密后的ascll码
+      let charCode = parseInt(strCharList[i], hex);
+      // 异或解密出原字符的ascll码
+      charCode = (charCode * 1) ^ xor;
+      let strChar = String.fromCharCode(charCode);
+      resultList.push(strChar);
+    }
+    let resultStr = resultList.join('');
+    return resultStr;
+}
+
+// 如何使用
+var pwd="BLUE123456!";
+var enPwd=encrypto(pwd,123,25);
+var dePwd=decrypto(enPwd,123,25);
+console.log(enPwd);//27z25z1lz2cz2oz2nz2mz34z33z32z3f
+console.log(dePwd);//BLUE123456!
+
+
 
 
 
 /*
 
 
-			合并其他分支上指定的文件或者文件夹到当前分支
+	一、合并其他分支上指定的文件或者文件夹到当前分支的2个命令
 
-			git checkout branchName folderName
-			git checkout branchName path
+	A：git checkout branchName folderName(合并指定分支上文件夹到当前分支)
+	B：git checkout branchName path(合并指定分支上某个文件下的某个文件到当前路径)
 
-			注：一下都是在主分支master上执行的命令
-			1 把dev1 分支上app下所有的文件合并到主分支master上.
-			git checkout dev app
+	二、如何使用：以下都是在主分支master上执行的命令
+	1 把dev1 分支上app下所有的文件合并到主分支master上.
+	git checkout dev app
 
-			2 部分更新，如单独合并app/css/index.css到master主分支上.
-			git checkout dev app/css/index.css
+	2 部分更新，如单独合并app/css/index.css到master主分支上.
+	git checkout dev app/css/index.css
 
-			3 部分文件夹dev分支上app的js文件夹下有多个JS文件都更新了.
-			git checkout dev app/js
+	3 部分文件夹dev分支上app的js文件夹下有多个JS文件都更新了.
+	git checkout dev app/js
 
-			合并过来的文件或者文件夹在主分支master上都是默认add过的，
-			然后需要在master分支上commit,再push即可完成合并更新！
+	合并过来的文件或者文件夹在主分支master上都是默认add过的，
+	然后需要在master分支上commit,再push即可完成合并更新！
 
 
 
