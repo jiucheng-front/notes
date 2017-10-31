72a5f390fb42fd95e94ed5dca971a151a09839a1
wangjianfei <444128852@qq.com>
2017-10-31 14:27:47 +0800

    replace the babel-preset2015 to babel-preset-env

---
+++ react/cli/comment/src/components/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import logo from '../assets/logo.svg';
+import '../styles/App.css';
+import "../styles/fontstyle.css"
+
+import Toogle from "./Toogle";
+
+//icon列表
+import Icon from "./Icon.jsx"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/entry.js</code> and save to reload.
+        </p>
+        <h3>开关小组件</h3>
+        <Toogle />
+        <h3>Icon字体，JSX</h3>
+        <Icon />
+      </div>
+    );
+  }
+}
+
+export default App;
