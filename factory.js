13624c4a440266ddd8561945bdb6a9b87771c946
wangjianfei <444128852@qq.com>
2017-11-17 10:07:44 +0800

    add arrayEqual handle

--- factory.js
+++ factory.js
@@ -377,6 +377,15 @@ console.log(dePwd);//BLUE123456!
  }
 
 
+//  十、判断两个数组是否相等
+function arrayEqual(arr1, arr2) {
+	if(arr1 === arr2) return true;
+	if(arr1.length != arr2.length) return false;
+	for(vari = 0; i < arr1.length; ++i) {
+		if(arr1[i] !== arr2[i]) return false;
+	}
+	return true;
+}
 
 
 
