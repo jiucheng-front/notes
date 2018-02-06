a7e398d06fc5692474d548949ed827329a1a7603
wangjianfei <444128852@qq.com>
2018-02-06 14:57:32 +0800

    add sigbstatus true

---
+++ es6class.js
@@ -0,0 +1,42 @@
+"use strict";
+class Video {
+    constructor(signStatus){
+        this.signStatus = signStatus
+        this.boxDom = document.getElementById("boxDom")
+        this.init()
+    }
+    init(){
+        this._getAnchorInfo()
+    }
+    _getAnchorInfo(){
+        api.getInfo("/anchor/info").then(res =>{
+            // success
+            if(res.ret_code=="0"){
+                let data = res.data
+                this._renderHtml(data)
+            // failed
+            }else{
+                this._errorTips()
+            }
+        }).catch(error =>{
+            console.log(error)
+        })
+    }
+    _renderHtml(data){
+        if(this.signStatus){
+            // 存在
+        }else{
+            this.boxDom.innerHTML="</p>"+data+"</p>"
+        }
+    }
+    _errorTips(){
+        this.boxDom.innerHTML="数据为空了~"
+    }
+}
+
+var ReplayVideos = new Video()
+window.ReplayVideos = ReplayVideos
+
+// other status
+
+// var ReplayVideos = new Video(true)
